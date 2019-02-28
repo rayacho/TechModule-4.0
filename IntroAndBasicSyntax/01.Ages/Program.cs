6d7344338391b6d769be1e83bc93a7c200930d63
rayacho <shawnsgf@abv.bg>
2019-02-28 13:05:05 +0200

    added ex.1 ages

---
+++ IntroAndBasicSyntax/01.Ages/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01.Ages
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int age = int.Parse(Console.ReadLine());
+			bool baby = age >= 0 && age <= 2;
+			bool child = age >= 3 && age <= 13;
+			bool teenager = age >= 14 && age <= 19;
+			bool adult = age >= 20 && age <= 65;
+			bool elder = age >= 66;
+
+			if(baby)
+			{
+				Console.WriteLine("baby");
+			}
+			else if(child)
+			{
+				Console.WriteLine("child");
+			}
+			else if(teenager)
+			{
+				Console.WriteLine("teenager");
+			}
+			else if(adult)
+			{
+				Console.WriteLine("adult");
+			}
+			else if(elder)
+			{
+				Console.WriteLine("elder");
+			}
+		}
+	}
+}
