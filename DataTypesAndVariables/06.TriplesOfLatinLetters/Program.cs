2849413377ea2a29282d8924c1a0a66ffbdd2d4f
rayacho <shawnsgf@abv.bg>
2019-02-28 14:06:53 +0200

    added ex.6 triples of latin letters

---
+++ DataTypesAndVariables/06.TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _06.TriplesOfLatinLetters
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int letters = int.Parse(Console.ReadLine());
+			char character;
+
+			for(int i = 0; i < letters; i++)
+			{
+				for(int j = 0; j < letters; j++)
+				{
+					for(int k = 0; k < letters; k++)
+					{
+						character = (char)('a' + i);
+						Console.Write(character);
+						character = (char)('a' + j);
+						Console.Write(character);
+						character = (char)('a' + k);
+						Console.Write(character);
+						Console.WriteLine();
+					}
+				}
+			}
+		}
+	}
+}
