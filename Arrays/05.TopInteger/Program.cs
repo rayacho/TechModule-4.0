3b235123c3faf2869236ccc6af34bda6009c8deb
rayacho <shawnsgf@abv.bg>
2019-02-28 14:24:46 +0200

    added ex.5 top integer

---
+++ Arrays/05.TopInteger/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.TopInteger
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+			int j = 0;
+
+			foreach (int number in array)
+			{
+				bool top = true;
+
+				for(int i = j + 1; i < array.Length; i++)
+				{
+					if(number <= array[i])
+					{
+						top = false;
+					}
+				}
+
+				if(top)
+				{
+					Console.Write(number + " ");
+				}
+
+				j++;
+			}
+		}
+	}
+}
