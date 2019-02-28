3b6145f293e90413d2fceae54df54c03f65956b9
rayacho <shawnsgf@abv.bg>
2019-02-28 14:21:14 +0200

    added ex.2 common elements

---
+++ Arrays/02.CommonElements/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02.CommonElements
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] first = Console.ReadLine().Split();
+			string[] second = Console.ReadLine().Split();
+
+			for (int i = 0; i < first.Length; i++)
+			{
+				for (int j = 0; j < second.Length; j++)
+				{
+					if (second[j] == first[i])
+					{
+						Console.Write(second[j] + " ");
+					}
+				}
+			}
+		}
+	}
+}
