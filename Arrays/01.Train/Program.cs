2548ce1b3e3cf22e047e1b93712f736391dca29a
rayacho <shawnsgf@abv.bg>
2019-02-28 14:17:40 +0200

    added ex.1 train

---
+++ Arrays/01.Train/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01.Train
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int number = int.Parse(Console.ReadLine());
+			int[] train = new int[number];
+			int sum = 0;
+
+			for(int i = 0; i < number; i++)
+			{
+				int vagon = int.Parse(Console.ReadLine());
+				train[i] = vagon;
+				sum += vagon;
+			}
+
+			foreach (int vagon in train)
+			{
+				Console.Write(vagon + " ");
+			}
+
+			Console.WriteLine();
+			Console.WriteLine(sum);
+		}
+	}
+}
