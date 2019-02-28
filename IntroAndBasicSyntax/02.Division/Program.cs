e5aa8cb5b481c67274b366c3d47e71f30a7275d2
rayacho <shawnsgf@abv.bg>
2019-02-28 13:02:13 +0200

    add ex 2 division

---
+++ IntroAndBasicSyntax/02.Division/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _02.Division
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int number = int.Parse(Console.ReadLine());
+			bool ten = number % 10 == 0;
+			bool seven = number % 7 == 0;
+			bool six = number % 6 == 0;
+			bool three = number % 3 == 0;
+			bool two = number % 2 == 0;
+
+			if(ten)
+			{
+				Console.WriteLine($"The number is divisible by 10");
+			}
+			else if(seven)
+			{
+				Console.WriteLine($"The number is divisible by 7");
+			}
+			else if(six)
+			{
+				Console.WriteLine($"The number is divisible by 6");
+			}
+			else if(three)
+			{
+				Console.WriteLine($"The number is divisible by 3");
+			}
+			else if(two)
+			{
+				Console.WriteLine($"The number is divisible by 2");
+			}
+			else
+			{
+				Console.WriteLine("Not divisible");
+			}
+		}
+	}
+}
