957b5778306ef11f5fdb41d82ae338f9f7f89f84
rayacho <shawnsgf@abv.bg>
2019-02-28 14:10:18 +0200

    added ex.8 beer kegs

---
+++ DataTypesAndVariables/08.BeerKegs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08.BeerKegs
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int kegs = int.Parse(Console.ReadLine());
+			double max = 0;
+			string maxModel = "";
+
+			for (int i = 0; i < kegs; i++)
+			{
+				string model = Console.ReadLine();
+				double radius = double.Parse(Console.ReadLine());
+				int height = int.Parse(Console.ReadLine());
+				double volume = Math.PI * radius * radius * height;
+
+				if (volume > max)
+				{
+					max = volume;
+					maxModel = model;
+				}
+			}
+
+			Console.WriteLine(maxModel);
+		}
+	}
+}
