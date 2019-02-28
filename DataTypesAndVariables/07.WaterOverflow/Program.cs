6bd4a256424ee8278dadfcc706f57e3eea79b51a
rayacho <shawnsgf@abv.bg>
2019-02-28 14:08:19 +0200

    added ex.7 water overflow

---
+++ DataTypesAndVariables/07.WaterOverflow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07.WaterOverflow
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int capacity = 255;
+			int lines = int.Parse(Console.ReadLine());
+			int sumCapacity = 0;
+			
+			for(int i = 0; i < lines; i++)
+			{
+				int inputCapacity = int.Parse(Console.ReadLine());
+				if(inputCapacity > capacity)
+				{
+					Console.WriteLine("Insufficient capacity!");
+				}
+				else
+				{
+					capacity -= inputCapacity;
+					sumCapacity += inputCapacity;
+				}
+			}
+
+			Console.WriteLine(sumCapacity);
+		}
+	}
+}
