cd6457cd6de062ee8bddee48619506a38a7a52c3
rayacho <shawnsgf@abv.bg>
2019-02-28 14:23:49 +0200

    added ex.4 array rotation

---
+++ Arrays/04.ArrayRotation/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.ArrayRotation
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+			int startIndex = int.Parse(Console.ReadLine());
+
+			if(startIndex > array.Length)
+			{
+				startIndex = startIndex - array.Length;
+
+				for(int i = startIndex; i < array.Length; i++)
+				{
+					Console.Write(array[i] + " ");
+				}
+
+				for(int i = 0; i < startIndex; i++)
+				{
+					Console.Write(array[i] + " ");
+				}
+			}
+			else
+			{
+				for (int i = startIndex; i < array.Length; i++)
+				{
+					Console.Write(array[i] + " ");
+				}
+
+				for (int i = 0; i < startIndex; i++)
+				{
+					Console.Write(array[i] + " ");
+				}
+			}
+		}
+	}
+}
