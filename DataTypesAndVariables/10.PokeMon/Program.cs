cf31915c03d74475b4af6419b7415ac4c26b318b
rayacho <shawnsgf@abv.bg>
2019-02-28 14:12:25 +0200

    added ex.10 pokemon

---
+++ DataTypesAndVariables/10.PokeMon/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _10.PokeMon
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int pokePower = int.Parse(Console.ReadLine());
+			int distance = int.Parse(Console.ReadLine());
+			int exhaustionFactor = int.Parse(Console.ReadLine());
+			int halfPokePower = pokePower / 2;
+			int count = 0;
+
+			while (pokePower >= distance)
+			{
+				count++;
+				pokePower -= distance;
+
+				if (pokePower == halfPokePower && exhaustionFactor != 0)
+				{
+					if (pokePower / exhaustionFactor != 0)
+					{
+						pokePower /= exhaustionFactor;
+					}
+				}	
+			}
+
+			Console.WriteLine(pokePower);
+			Console.WriteLine(count);
+		}
+	}
+}
