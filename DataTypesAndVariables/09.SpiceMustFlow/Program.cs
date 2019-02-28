e6d852a08cde01eb912fe03fdc44861636c1b117
rayacho <shawnsgf@abv.bg>
2019-02-28 14:11:17 +0200

    added ex.9 spice must flow

---
+++ DataTypesAndVariables/09.SpiceMustFlow/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _09.SpiceMustFlow
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int start = int.Parse(Console.ReadLine());
+			int days = 0;
+			int consume = 26;
+			int spices = 0;
+
+			while (start >= 100)
+			{
+				if(start <= consume)
+				{
+					start = 0;
+				}
+
+				days++;
+				spices = spices + start - consume;
+				start = start - 10;
+			}
+
+			if (start < 100)
+			{
+				if (spices >= consume)
+				{
+					spices -= consume;
+				}
+			}
+			else
+			{
+				Console.WriteLine("Yield cannot be null");
+				return;
+			}
+
+			Console.WriteLine(days);
+			Console.WriteLine(spices);
+		}
+	}
+}
