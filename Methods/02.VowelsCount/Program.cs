1646b36194061e38eb6a126a0b82adefdba6ab33
rayacho <shawnsgf@abv.bg>
2019-03-01 12:37:46 +0200

    added ex.2 vowels count

---
+++ Methods/02.VowelsCount/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _02.VowelsCount
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+			input = input.ToLower();
+			int count = 0;
+
+			foreach (char symbol in input)
+			{
+				if(IsVowel(symbol))
+				{
+					count++;
+				}
+			}
+
+			Console.WriteLine(count);
+		}
+
+		static bool IsVowel(char a)
+		{
+			if(a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'y' || a == 'u')
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
