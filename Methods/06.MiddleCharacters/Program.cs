5506a26eadec6ec8a07cf39690645cc3ccb82855
rayacho <shawnsgf@abv.bg>
2019-03-01 12:42:18 +0200

    added ex.6 middle characters

---
+++ Methods/06.MiddleCharacters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _06.MiddleCharacters
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+			Console.WriteLine(MiddleCharacter(input));
+		}
+
+		static string MiddleCharacter(string str)
+		{
+			if(str.Length % 2 == 0)
+			{
+				int firstIndex = str.Length / 2 - 1;
+				int secondIndex = str.Length / 2;
+				string output = $"{str[firstIndex]}{str[secondIndex]}";
+				return output;
+			}
+			else
+			{
+				int middleIndex = str.Length / 2;
+				string output = $"{str[middleIndex]}";
+				return output;
+			}
+		}
+	}
+}
