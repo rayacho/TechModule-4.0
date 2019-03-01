83a060d3f5d30b1d72f6c11e15cd328eae42d2db
rayacho <shawnsgf@abv.bg>
2019-03-01 12:47:26 +0200

    added ex.10 top number

---
+++ Methods/10.TopNumber/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _10.TopNumber
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			for (int i = 1; i < n; i++)
+			{
+				if(IsDivisibleBy8(i) && OddDigit(i))
+				{
+					Console.WriteLine(i);
+				}
+			}
+		}
+
+		static bool IsDivisibleBy8(int number)
+		{
+			string numberArray = number.ToString();
+			int sum = 0;
+			foreach (char digit in numberArray)
+			{
+				sum += digit;
+			}
+
+			if(sum % 8 == 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		static bool OddDigit(int number)
+		{
+			string numberArray = number.ToString();
+			bool odd = false;
+			foreach (char digit in numberArray)
+			{
+				if (digit % 2 != 0)
+				{
+					odd = true;
+				}
+			}
+
+			return odd;
+		}
+	}
+}
