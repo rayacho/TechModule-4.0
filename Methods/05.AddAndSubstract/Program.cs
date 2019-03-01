dba8b5bc7e5964ec80b63fcba14a8829800a215e
rayacho <shawnsgf@abv.bg>
2019-03-01 12:41:00 +0200

    added ex.5 add and substract

---
+++ Methods/05.AddAndSubstract/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05.AddAndSubstract
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int first = int.Parse(Console.ReadLine());
+			int second = int.Parse(Console.ReadLine());
+			int third = int.Parse(Console.ReadLine());
+
+			int sumFirstAndSecond = Sum(first, second);
+			int substraction = Substract(sumFirstAndSecond, third);
+
+			Console.WriteLine(substraction);
+		}
+
+		static int Sum(int a, int b)
+		{
+			return a + b;
+		}
+
+		static int Substract(int a, int b)
+		{
+			return a - b;
+		}
+	}
+}
