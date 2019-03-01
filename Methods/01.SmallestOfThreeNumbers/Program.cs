b01f44403fe8666c22604874578ea623790fe7c4
rayacho <shawnsgf@abv.bg>
2019-03-01 12:36:40 +0200

    added ex.1 smallest of three numbers

---
+++ Methods/01.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01.SmallestOfThreeNumbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int first = int.Parse(Console.ReadLine());
+			int second = int.Parse(Console.ReadLine());
+			int third = int.Parse(Console.ReadLine());
+
+			Console.WriteLine(SmallestNumber(first, second, third));
+		}
+		static int SmallestNumber(int a, int b, int c)
+		{
+			int min = a;
+			if(b < min)
+			{
+				min = b;
+			}
+			if(c < min)
+			{
+				min = c;
+			}
+
+			return min;
+		}
+	}
+}
