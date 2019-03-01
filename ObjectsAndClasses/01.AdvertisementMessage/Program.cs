e0d110d82aff5da01130abbe24f53c007da0a517
rayacho <shawnsgf@abv.bg>
2019-03-01 13:39:31 +0200

    finished ex.1 advertisement message

--- ObjectsAndClasses/01.AdvertisementMessage/Program.cs
+++ ObjectsAndClasses/01.AdvertisementMessage/Program.cs
@@ -6,7 +6,33 @@ namespace _01.AdvertisementMessage
 	{
 		static void Main(string[] args)
 		{
-			
+			string[] phrases = { "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.",
+				"Exceptional product.", "I can’t live without this product." };
+			string[] events = { "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!",
+				"I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" };
+			string[] authors = { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+			string[] cities = { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+
+			int n = int.Parse(Console.ReadLine());
+
+			for(int i = 0; i < n; i++)
+			{
+				Random random = new Random();
+
+				int phrasePosition = random.Next(phrases.Length);
+				string phrase = phrases[phrasePosition];
+
+				int eventPosition = random.Next(events.Length);
+				string currentEvent = events[eventPosition];
+
+				int authorPosition = random.Next(authors.Length);
+				string author = authors[authorPosition];
+
+				int cityPosition = random.Next(cities.Length);
+				string city = cities[cityPosition];
+
+				Console.WriteLine($"{phrase} {currentEvent} {author} – {city}");
+			}
 		}
 	}
 }
