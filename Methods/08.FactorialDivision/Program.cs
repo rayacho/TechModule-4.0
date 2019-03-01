4785ccfd14438eb5bbe79fe8d5f1f0bd0e3fd2b9
rayacho <shawnsgf@abv.bg>
2019-03-01 12:44:46 +0200

    added ex.8 factorial division

---
+++ Methods/08.FactorialDivision/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _08.FactorialDivision
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			long first = long.Parse(Console.ReadLine());
+			long second = long.Parse(Console.ReadLine());
+			double division = Factorial(first) / Factorial(second);
+			string result = $"{division:F2}";
+
+			Console.WriteLine(result);
+		}
+
+		static double Factorial(double a)
+		{
+			if (a == 0)
+			{
+				return 1;
+			}
+			else
+			{
+				int factorial = 1;
+
+				for (int i = 2; i <= a; i++)
+				{
+					factorial *= i;
+				}
+
+				return factorial;
+			}
+		}
+	}
+}
