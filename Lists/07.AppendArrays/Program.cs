1c9849e4a86e7c26f24dab9df1af918f8c9f6251
rayacho <shawnsgf@abv.bg>
2019-03-01 13:01:56 +0200

    added ex.7 append arrays

---
+++ Lists/07.AppendArrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.AppendArrays
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> input = Console.ReadLine().Split('|').Reverse().ToList();
+			List<int> numbers = new List<int>();
+
+			foreach (string array in input)
+			{
+				numbers.AddRange(array.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList());
+			}
+
+			foreach(int number in numbers)
+			{
+				Console.Write(number + " ");
+			}
+		}
+	}
+}
