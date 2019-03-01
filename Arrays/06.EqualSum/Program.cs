30328fb655b7d4193e6b6856f027d26053142fc4
rayacho <shawnsgf@abv.bg>
2019-03-01 12:23:16 +0200

    added ex.6 equal sum

---
+++ Arrays/06.EqualSum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.EqualSum
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				int leftSum = 0;
+				int rightSum = 0;
+
+				for (int j = 0; j < i; j++)
+				{
+					leftSum += numbers[j];
+				}
+
+				for (int j = i + 1; j < numbers.Length; j++)
+				{
+					rightSum += numbers[j];
+				}
+
+				if (leftSum == rightSum)
+				{
+					Console.WriteLine(i);
+					return;
+				}
+			}
+
+			Console.WriteLine("no");
+		}
+	}
+}
