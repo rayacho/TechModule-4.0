b94c00470424156c9dd8e4f026c4be0868732f00
rayacho <shawnsgf@abv.bg>
2019-03-01 12:46:25 +0200

    added ex.9 palindrome integers

---
+++ Methods/09.PalindomeIntegers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _09.PalindomeIntegers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string command;
+			while((command = Console.ReadLine()) != "END")
+			{
+				int number = int.Parse(command);
+				if(Palindrome(number))
+				{
+					Console.WriteLine("true");
+				}
+				else
+				{
+					Console.WriteLine("false");
+				}
+			}
+		}
+
+		static bool Palindrome(int number)
+		{
+			string forward = number.ToString();
+			char[] forwardArray = forward.ToCharArray();
+			string reverse = string.Empty;
+			for (int i = forwardArray.Length - 1; i > -1; i--)
+			{
+				reverse += forwardArray[i];
+			}
+
+			if(forward == reverse)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
