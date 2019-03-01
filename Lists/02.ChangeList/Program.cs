2d311cea50e7aaeeff494158d7324d4157bd3f79
rayacho <shawnsgf@abv.bg>
2019-03-01 12:54:42 +0200

    added ex.2 change list

---
+++ Lists/02.ChangeList/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.ChangeList
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+			List<int> elements = new List<int>();
+
+			foreach (int element in input)
+			{
+				elements.Add(element);
+			}
+
+			string command;
+
+			while ((command = Console.ReadLine()) != "end")
+			{
+				string[] array = command.Split().ToArray();
+				string action = array[0];
+
+				switch (action)
+				{
+					case "Delete":
+						int numberToDelete = int.Parse(array[1]);
+						elements.Remove(numberToDelete);
+						break;
+					case "Insert":
+						int numberToInsert = int.Parse(array[1]);
+						int position = int.Parse(array[2]);
+						elements.Insert(position, numberToInsert);
+						break;
+				}
+			}
+
+			foreach (int element in elements)
+			{
+				Console.Write(element + " ");
+			}
+		}
+	}
+}
