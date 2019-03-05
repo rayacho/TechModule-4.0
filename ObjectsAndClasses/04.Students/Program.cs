ea70211b16dd6a24d9d166ba49663fd3ad8e83ba
rayacho <shawnsgf@abv.bg>
2019-03-05 13:40:06 +0200

    added ex.4 students

---
+++ ObjectsAndClasses/04.Students/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Students
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Student> students = new List<Student>();
+			int numberOfStudents = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < numberOfStudents; i++)
+			{
+				string[] input = Console.ReadLine().Split(" ").ToArray();
+				string firstName = input[0];
+				string lastName = input[1];
+				double grade = double.Parse(input[2]);
+				Student student = new Student(firstName, lastName, grade);
+
+				students.Add(student);
+			}
+
+			students = students.OrderByDescending(x => x.Grade).ToList();
+
+			foreach (Student student in students)
+			{
+				Console.WriteLine(student.ToString());
+			}
+		}
+	}
+
+	public class Student
+	{
+		public string FirstName { get; set; }
+
+		public string LastName { get; set; }
+
+		public double Grade { get; set; }
+
+		public Student(string firstName, string lastName, double grade)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+			Grade = grade;
+		}
+
+		public override string ToString()
+		{
+			return $"{FirstName} {LastName}: {Grade:F2}";
+		}
+	}
+}
