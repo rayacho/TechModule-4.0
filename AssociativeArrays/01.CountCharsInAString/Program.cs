84140549221e9d6dc9a1e469324018efcd68052f
rayacho <shawnsgf@abv.bg>
2019-03-12 14:16:17 +0200

    added ex.1 count chars in a string

---
+++ AssociativeArrays/01.CountCharsInAString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.CountCharsInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> chars = new Dictionary<char, int>();
+            string input = Console.ReadLine();
+
+            foreach (char charElement in input)
+            {
+                if (charElement != ' ')
+                {
+                    if (chars.ContainsKey(charElement))
+                    {
+                        chars[charElement]++;
+                    }
+                    else
+                    {
+                        chars.Add(charElement, 1);
+                    }
+                }
+            }
+
+            foreach (var pair in chars)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
