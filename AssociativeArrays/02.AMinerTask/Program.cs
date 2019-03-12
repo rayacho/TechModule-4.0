6ab88b429510838d02a4077ad7b414eea318cfba
rayacho <shawnsgf@abv.bg>
2019-03-12 14:17:50 +0200

    added ex.2 a miner task

---
+++ AssociativeArrays/02.AMinerTask/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.AMinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> resources = new Dictionary<string, double>();
+            string resource = Console.ReadLine();
+
+            while (resource != "stop")
+            {
+                double quality = double.Parse(Console.ReadLine());
+                if (!resources.ContainsKey(resource))
+                {
+                    resources.Add(resource, quality);
+                }
+                else
+                {
+                    resources[resource] += quality;
+                }
+
+                resource = Console.ReadLine();
+            }
+
+            foreach (var miner in resources)
+            {
+                Console.WriteLine($"{miner.Key} -> {miner.Value}");
+            }
+        }
+    }
+}
