17ae7eadc20d2c145068ea6a1c685fa9b4b66305
rayacho <shawnsgf@abv.bg>
2019-03-14 13:54:08 +0200

    added ex.8 company users

---
+++ AssociativeArrays/08.CompanyUsers/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.CompanyUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> employ = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "End")
+                {
+                    break;
+
+                }
+
+                string[] tokens = command.Split(" -> ");
+                string companyName = tokens[0];
+                string employId = tokens[1];
+
+                if (!employ.ContainsKey(companyName))
+                {
+                    employ[companyName] = new List<string>();
+                    employ[companyName].Add(employId);
+                }
+                else if (employ[companyName].Contains(employId))
+                {
+                    continue;
+                }
+                else
+                {
+                    employ[companyName].Add(employId);
+                }
+
+            }
+
+            foreach (var item in employ.OrderBy(x => x.Key))
+            {
+                Console.WriteLine(item.Key);
+                var sorted = item.Value;
+
+                foreach (var employId in sorted)
+                {
+                    Console.WriteLine($"-- {employId}");
+                }
+            }
+        }
+    }
+}
