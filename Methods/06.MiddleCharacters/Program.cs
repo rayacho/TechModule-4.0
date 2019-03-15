dd35d9327439fdf47831b2d61261b7a91365db9b
rayacho <shawnsgf@abv.bg>
2019-03-15 14:11:06 +0200

    modified a little bit more

--- Methods/06.MiddleCharacters/Program.cs
+++ Methods/06.MiddleCharacters/Program.cs
@@ -2,30 +2,30 @@
 
 namespace _06.MiddleCharacters
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			string input = Console.ReadLine();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
 
-			Console.WriteLine(MiddleCharacter(input));
-		}
+            Console.WriteLine(MiddleCharacter(input));
+        }
 
-		static string MiddleCharacter(string str)
-		{
-			if(str.Length % 2 == 0)
-			{
-				int firstIndex = str.Length / 2 - 1;
-				int secondIndex = str.Length / 2;
-				string output = $"{str[firstIndex]}{str[secondIndex]}";
-				return output;
-			}
-			else
-			{
-				int middleIndex = str.Length / 2;
-				string output = $"{str[middleIndex]}";
-				return output;
-			}
-		}
-	}
+        static string MiddleCharacter(string str)
+        {
+            if (str.Length % 2 == 0)
+            {
+                int firstIndex = str.Length / 2 - 1;
+                int secondIndex = str.Length / 2;
+                string output = $"{str[firstIndex]}{str[secondIndex]}";
+                return output;
+            }
+            else
+            {
+                int middleIndex = str.Length / 2;
+                string output = $"{str[middleIndex]}";
+                return output;
+            }
+        }
+    }
 }
