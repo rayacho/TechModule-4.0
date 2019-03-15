d02529d20d6aaaf2620341cbdfe0335009469a35
rayacho <shawnsgf@abv.bg>
2019-03-15 12:53:41 +0200

    modified ex.3 vacation

--- IntroAndBasicSyntax/03.Vacation/Program.cs
+++ IntroAndBasicSyntax/03.Vacation/Program.cs
@@ -2,108 +2,120 @@
 
 namespace _03.Vacation
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			int people = int.Parse(Console.ReadLine());
-			string type = Console.ReadLine();
-			string day = Console.ReadLine();
-			double money = 0;
-			double price;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            string type = Console.ReadLine();
+            string day = Console.ReadLine();
+            double money = 0;
+            double price;
 
-			switch (day)
-			{
-				case "Friday":
-					if (type == "Students")
-					{
-						price = 8.45;
-						money = price * people;
-						if (people >= 30)
-						{
-							money = money - (money * 0.15);
-						}
-					}
-					else if (type == "Business")
-					{
-						price = 10.90;
-						money = price * people;
-						if (people >= 100)
-						{
-							money = money - (10 * price);
-						}
-					}
-					else if (type == "Regular")
-					{
-						price = 15;
-						money = people * price;
-						if (people >= 10 && people <= 20)
-						{
-							money = money - (0.05 * money);
-						}
-					}
-					break;
-				case "Saturday":
-					if (type == "Students")
-					{
-						price = 9.80;
-						money = price * people;
-						if (people >= 30)
-						{
-							money = money - (money * 0.15);
-						}
-					}
-					else if (type == "Business")
-					{
-						price = 15.60;
-						money = price * people;
-						if (people >= 100)
-						{
-							money = money - (10 * price);
-						}
-					}
-					else if (type == "Regular")
-					{
-						price = 20;
-						money = people * price;
-						if (people >= 10 && people <= 20)
-						{
-							money = money - (0.05 * money);
-						}
-					}
-					break;
-				case "Sunday":
-					if (type == "Students")
-					{
-						price = 10.46;
-						money = price * people;
-						if (people >= 30)
-						{
-							money = money - (money * 0.15);
-						}
-					}
-					else if (type == "Business")
-					{
-						price = 16;
-						money = price * people;
-						if (people >= 100)
-						{
-							money = money - (10 * price);
-						}
-					}
-					else if (type == "Regular")
-					{
-						price = 22.50;
-						money = people * price;
-						if (people >= 10 && people <= 20)
-						{
-							money = money - (0.05 * money);
-						}
-					}
-					break;
-			}
+            switch (day)
+            {
+                case "Friday":
+                    if (type == "Students")
+                    {
+                        price = 8.45;
+                        money = price * people;
+                        if (people >= 30)
+                        {
+                            money = money - (money * 0.15);
+                        }
+                    }
+                    else if (type == "Business")
+                    {
+                        price = 10.90;
+                        money = price * people;
+                        if (people >= 100)
+                        {
+                            money = money - (10 * price);
+                        }
+                    }
+                    else if (type == "Regular")
+                    {
+                        price = 15;
+                        money = people * price;
+                        if (people >= 10 && people <= 20)
+                        {
+                            money = money - (0.05 * money);
+                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentException();
+                    }
+                    break;
+                case "Saturday":
+                    if (type == "Students")
+                    {
+                        price = 9.80;
+                        money = price * people;
+                        if (people >= 30)
+                        {
+                            money = money - (money * 0.15);
+                        }
+                    }
+                    else if (type == "Business")
+                    {
+                        price = 15.60;
+                        money = price * people;
+                        if (people >= 100)
+                        {
+                            money = money - (10 * price);
+                        }
+                    }
+                    else if (type == "Regular")
+                    {
+                        price = 20;
+                        money = people * price;
+                        if (people >= 10 && people <= 20)
+                        {
+                            money = money - (0.05 * money);
+                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentException();
+                    }
+                    break;
+                case "Sunday":
+                    if (type == "Students")
+                    {
+                        price = 10.46;
+                        money = price * people;
+                        if (people >= 30)
+                        {
+                            money = money - (money * 0.15);
+                        }
+                    }
+                    else if (type == "Business")
+                    {
+                        price = 16;
+                        money = price * people;
+                        if (people >= 100)
+                        {
+                            money = money - (10 * price);
+                        }
+                    }
+                    else if (type == "Regular")
+                    {
+                        price = 22.50;
+                        money = people * price;
+                        if (people >= 10 && people <= 20)
+                        {
+                            money = money - (0.05 * money);
+                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentException();
+                    }
+                    break;
+            }
 
-			Console.WriteLine($"Total price: {money:F2}");
-		}
-	}
+            Console.WriteLine($"Total price: {money:F2}");
+        }
+    }
 }
