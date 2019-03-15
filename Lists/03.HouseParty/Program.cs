e53fa2a30ccbb9b24474f32b64c886f8c15106bd
rayacho <shawnsgf@abv.bg>
2019-03-15 14:52:13 +0200

    added new lines

--- Lists/03.HouseParty/Program.cs
+++ Lists/03.HouseParty/Program.cs
@@ -10,11 +10,13 @@ namespace _03.HouseParty
 			List<string> guests = new List<string>();
 			int number = int.Parse(Console.ReadLine());
 			List<string> output = new List<string>();
+
 			for(int i = 0; i < number; i++)
 			{
 				string[] guest = Console.ReadLine().Split();
 				string name = guest[0];
 				string going = guest[2];
+
 				if(going == "going!")
 				{
 					if(guests.Contains(name))
