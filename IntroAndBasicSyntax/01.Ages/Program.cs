cc47c70434aa779143d6387cf2ca077af534e030
rayacho <shawnsgf@abv.bg>
2019-03-15 12:46:24 +0200

    modified ex.1 ages

--- IntroAndBasicSyntax/01.Ages/Program.cs
+++ IntroAndBasicSyntax/01.Ages/Program.cs
@@ -2,37 +2,41 @@
 
 namespace _01.Ages
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			int age = int.Parse(Console.ReadLine());
-			bool baby = age >= 0 && age <= 2;
-			bool child = age >= 3 && age <= 13;
-			bool teenager = age >= 14 && age <= 19;
-			bool adult = age >= 20 && age <= 65;
-			bool elder = age >= 66;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            bool baby = age >= 0 && age <= 2;
+            bool child = age >= 3 && age <= 13;
+            bool teenager = age >= 14 && age <= 19;
+            bool adult = age >= 20 && age <= 65;
+            bool elder = age >= 66;
 
-			if(baby)
-			{
-				Console.WriteLine("baby");
-			}
-			else if(child)
-			{
-				Console.WriteLine("child");
-			}
-			else if(teenager)
-			{
-				Console.WriteLine("teenager");
-			}
-			else if(adult)
-			{
-				Console.WriteLine("adult");
-			}
-			else if(elder)
-			{
-				Console.WriteLine("elder");
-			}
-		}
-	}
+            if (baby)
+            {
+                Console.WriteLine("baby");
+            }
+            else if (child)
+            {
+                Console.WriteLine("child");
+            }
+            else if (teenager)
+            {
+                Console.WriteLine("teenager");
+            }
+            else if (adult)
+            {
+                Console.WriteLine("adult");
+            }
+            else if (elder)
+            {
+                Console.WriteLine("elder");
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
 }
