af55c5b148f473ea9a649a00b54ea19e7a200f14
rayacho <shawnsgf@abv.bg>
2019-03-15 14:41:49 +0200

    added new lines

--- Methods/10.TopNumber/Program.cs
+++ Methods/10.TopNumber/Program.cs
@@ -7,6 +7,7 @@ namespace _10.TopNumber
 		static void Main(string[] args)
 		{
 			int n = int.Parse(Console.ReadLine());
+
 			for (int i = 1; i < n; i++)
 			{
 				if(IsDivisibleBy8(i) && OddDigit(i))
@@ -20,6 +21,7 @@ namespace _10.TopNumber
 		{
 			string numberArray = number.ToString();
 			int sum = 0;
+
 			foreach (char digit in numberArray)
 			{
 				sum += digit;
@@ -39,6 +41,7 @@ namespace _10.TopNumber
 		{
 			string numberArray = number.ToString();
 			bool odd = false;
+
 			foreach (char digit in numberArray)
 			{
 				if (digit % 2 != 0)
