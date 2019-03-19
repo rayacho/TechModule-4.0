b39dbbedfab90efd4b8f072bb3d61ad0c89d535e
rayacho <shawnsgf@abv.bg>
2019-03-19 14:13:52 +0200

    added ex.5 multiply big number

---
+++ TextProcessingAndRegularExpressions/05.MultiplyBigNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Numerics;
+
+namespace _05.MultiplyBigNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger bigNumber = BigInteger.Parse(Console.ReadLine());
+            int numberToMultiplyWith = int.Parse(Console.ReadLine());
+
+            BigInteger result = bigNumber * numberToMultiplyWith;
+            Console.WriteLine(result);
+        }
+    }
+}
