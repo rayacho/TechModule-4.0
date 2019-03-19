bd92e03276503770fc91e39d23abcbce240a5f9a
rayacho <shawnsgf@abv.bg>
2019-03-19 13:56:00 +0200

    added ex.1 valid usernames

---
+++ TextProcessingAndRegularExpressions/01.ValidUsernames/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ValidUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(", ").ToArray();
+
+            foreach (string word in words)
+            {
+                bool valid = true;
+
+                if (word.Length < 3 || word.Length > 16)
+                {
+                    valid = false;
+                }
+
+                foreach (char character in word)
+                {
+                    if (!char.IsLetterOrDigit(character) && character != '_' && character != '-')
+                    {
+                        valid = false;
+                    }
+                }
+
+                if (valid)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
+}
