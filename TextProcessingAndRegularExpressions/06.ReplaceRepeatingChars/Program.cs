228f691ce94621b39c2b4efe25a5f7ff5f54d772
rayacho <shawnsgf@abv.bg>
2019-03-19 14:24:39 +0200

    added ex.6 replace repeating chars

---
+++ TextProcessingAndRegularExpressions/06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.ReplaceRepeatingChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+            List<char> word = new List<char>();
+
+            foreach (char character in input)
+            {
+                char previousCharacter = word[word.Count - 1];
+
+                if(!word.Contains(character) || character != previousCharacter)
+                {
+                    word.Add(character);
+                }
+            }
+
+            string result = string.Join("", word);
+            Console.WriteLine(result);
+        }
+    }
+}
