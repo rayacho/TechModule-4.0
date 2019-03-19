c3bbe3159b8badc962292edabaa2f12c7e4c241c
rayacho <shawnsgf@abv.bg>
2019-03-19 14:02:02 +0200

    added ex.3 extract file

---
+++ TextProcessingAndRegularExpressions/03.ExtractFile/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03.ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] filePath = Console.ReadLine().Split('\\', '.');
+            string fileName = filePath[filePath.Length - 2];
+            string fileExtension = filePath[filePath.Length - 1];
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtension}");
+        }
+    }
+}
