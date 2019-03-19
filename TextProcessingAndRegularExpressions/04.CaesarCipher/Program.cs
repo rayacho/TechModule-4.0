18456a1bcb3fb0fa7d8da6411dfa9b7124392f56
rayacho <shawnsgf@abv.bg>
2019-03-19 14:03:59 +0200

    added ex.4 caesar cipher

---
+++ TextProcessingAndRegularExpressions/04.CaesarCipher/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04.CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] text = Console.ReadLine().ToCharArray();
+            string result = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                result += (char)(text[i] + 3);
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
