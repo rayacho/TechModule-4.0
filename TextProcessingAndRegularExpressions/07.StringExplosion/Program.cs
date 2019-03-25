2316e515cddedb189ffbf6496934576c78339e3e
rayacho <shawnsgf@abv.bg>
2019-03-25 13:53:58 +0200

    added ex.7 string explosion

---
+++ TextProcessingAndRegularExpressions/07.StringExplosion/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _07.StringExplosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            List<string> input = text
+                .Split(new char[] { '>' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            int currentLength = 0;
+            int start = 0;
+
+            if (text[0] == '>')
+            {
+                start = 0;
+            }
+            else
+            {
+                start = 1;
+            }
+
+            for (int i = start; i < input.Count; i++)
+            {
+                char first = input[i].First();
+                int strength = int.Parse(first.ToString());
+
+                int length = input[i].Length;
+
+                input[i] = new string(input[i].Skip(strength + currentLength).ToArray());
+
+                if (strength + currentLength > length)
+                {
+                    currentLength = strength + currentLength - length;
+                }
+            }
+
+            if (start == 0)
+            {
+                Console.WriteLine('>' + string.Join(">", input));
+            }
+            else
+            {
+                Console.WriteLine(string.Join(">", input));
+            }
+        }
+    }
+}
