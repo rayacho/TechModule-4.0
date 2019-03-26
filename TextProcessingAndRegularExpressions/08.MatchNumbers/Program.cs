0a59065393eab5ea26101b217eff80ba17ff6f02
rayacho <shawnsgf@abv.bg>
2019-03-26 13:55:51 +0200

    added ex.8 match numbers

---
+++ TextProcessingAndRegularExpressions/08.MatchNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _08.MatchNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numbers = Console.ReadLine();
+            Regex regex = new Regex(@"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))");
+            MatchCollection collection = regex.Matches(numbers);
+
+            foreach (object item in collection)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
